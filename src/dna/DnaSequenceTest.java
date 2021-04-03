7972c2b227ca766fbeb51dd845247ae2e82e7ddc
MakutaTeridax <heel7284@gmail.com>
2021-04-02 18:04:24 -0700

    fixed the problem with code
    fixed some stuff and did some magical thinking

--- src/dna/DnaSequenceTest.java
+++ src/dna/DnaSequenceTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 public class DnaSequenceTest {
     DnaSequence dnaSequence;
     @Before
@@ -15,5 +17,23 @@ public class DnaSequenceTest {
     @Test
     public void transcribe() {
         // TODO: implement tests
+    	String dna = "GCTCGCAACATCGGATATACA";
+    	List<String> okay = dnaSequence.transcribe(dna);
+    	String output = "";
+    	String separater = "";
+    	for(String a: okay) {
+    		output = output + separater + a;
+    		separater = "-";
+    	}
+    	assertEquals("ala-arg-asn-lle-gly-tyr-thr", output);
+    	dna = "ATGCGCAACATCGGATATACATAG";
+    	okay = dnaSequence.transcribe(dna);
+    	output = "";
+    	separater = "";
+    	for(String a: okay) {
+    		output = output + separater + a;
+    		separater = "-";
+    	}
+    	assertEquals("met-arg-asn-lle-gly-tyr-thr-stop", output);
     }
 }
